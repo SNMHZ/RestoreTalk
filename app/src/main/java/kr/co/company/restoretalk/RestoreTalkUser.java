efccb23cec167c241e3725dea232ecf3f9bce9cf
SNMHZ <snmhz325@gmail.com>
2021-06-06 21:03:45 +0900

    feat: Load raw file by name

--- app/src/main/java/kr/co/company/restoretalk/RestoreTalkUser.java
+++ app/src/main/java/kr/co/company/restoretalk/RestoreTalkUser.java
@@ -7,9 +7,15 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +30,32 @@ public class RestoreTalkUser extends AppCompatActivity {
         String intentitem = intent.getStringExtra("filename");
         Toast.makeText(getApplicationContext(), intentitem, Toast.LENGTH_LONG).show();
 
+        //read raw txt
+        String rawFileData = readTxt( getRawResIdByName(intentitem) );
+        System.out.println(rawFileData);
+
+        String[] lines = rawFileData.split("\n");
+
+        for(String line: lines){
+            System.out.println(line);
+            System.out.println("^^");
+        }
+
+//        try{
+//            FileInputStream fis = openFileInput(intentitem+".txt");
+//            byte[] data = new byte[fis.available()];
+//            while (fis.read(data) != -1) {;} // fres 스트림을 읽어 data 버퍼 저장
+//                fis.close();
+//            System.out.println(new String(data));
+//            Toast.makeText(getApplicationContext(), new String(data), Toast.LENGTH_LONG).show();
+//        }
+//        catch (FileNotFoundException e) {
+//            System.out.println("\n\n\n\n\n\n");
+//            System.out.println("\n\n\n\n\n\n File Not Found \n\n\n\n\n\n\n\n\n\n\n");
+//            System.out.println("\n\n\n\n\n\n");
+//        }
+//        catch (Exception e) {System.out.println("\n\n\n\n\n\n");System.out.println("yes");System.out.println("\n\n\n\n\n\n");;}
+
         Button browseBtn = (Button)findViewById(R.id.room);
         browseBtn.setOnClickListener(new OnClickListener(){
             public void onClick(View v){
@@ -31,5 +63,36 @@ public class RestoreTalkUser extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private String readTxt(int id) {
+        String data = null;
+        InputStream inputStream = getResources().openRawResource(id);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        int i;
+        try {
+            i = inputStream.read();
+            while (i != -1) {
+                //if(i == 64) {byteArrayOutputStream.write('\n');byteArrayOutputStream.write('\n');}
+                //else
+                byteArrayOutputStream.write(i);
+                i = inputStream.read();
+            }
+            data = new String(byteArrayOutputStream.toByteArray(),"UTF8");
+
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    public int getRawResIdByName(String resName) {
+        String pkgName = this.getPackageName();
+        // Return 0 if not found.
+        int resID = this.getResources().getIdentifier(resName, "raw", pkgName);
+        //Log.i("AndroidVideoView", "Res Name: " + resName + "==> Res ID = " + resID);
+        return resID;
     }
 }
